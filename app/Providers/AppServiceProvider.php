b7f1fbf7ff52de7efe0782516e2feec784acc903
João Ramajo <joaoramajo744@gmail.com>
2026-01-04 16:22:32 -0300

    feat(AppServiceProvider): configura ambiente de produção para usar HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Expense;
 use App\Observers\ExpenseObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
