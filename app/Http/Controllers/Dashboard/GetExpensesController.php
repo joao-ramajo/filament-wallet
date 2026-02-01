e29cbb75481b66f4afb7e87aaa5257f745b3f0ab
João Ramajo <joaoramajo744@gmail.com>
2026-01-31 21:06:22 -0300

    Refactor/api only (#9)
    * chore: ajusta ordenacao de categorias
    
    * refactor: implementa rotas para api
    
    * feat: adiciona rota para listagem de despesas

---
+++ app/Http/Controllers/Dashboard/GetExpensesController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class GetExpensesController extends Controller
+{
+    public function __invoke()
+    {
+        $user = Auth::user();
+
+        $expenses = DB::table('expenses')
+            ->leftJoin('categories', 'expenses.category_id', '=', 'categories.id')  // join opcional
+            ->where('expenses.user_id', $user->id)
+            ->orderBy('expenses.created_at', 'desc')
+            ->select(
+                'expenses.id',
+                'expenses.title',
+                'categories.name as category',
+                'expenses.amount',
+                'expenses.payment_date',
+                'expenses.due_date',
+                'expenses.type',
+                'expenses.status',
+            )
+            ->get();
+
+        return response()
+            ->json([]);
+    }
+}
