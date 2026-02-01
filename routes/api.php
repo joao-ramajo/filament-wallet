19b00424e4261e9785448634f60f0e9e1bedd6ce
João Ramajo <joaoramajo744@gmail.com>
2026-01-31 21:05:39 -0300

    feat: adiciona rota para listagem de despesas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Dashboard\GetExpensesController;
 use App\Http\Controllers\Dashboard\GetSummaryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,9 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', RegisterController::class)->name('api.register');
 Route::post('/login', LoginController::class)->name('api.login');
-Route::get("/dashboard/summary", GetSummaryController::class)->middleware([
+Route::get('/dashboard/summary', GetSummaryController::class)->middleware([
     'auth:sanctum'
 ])->name('api.get-summary');
+Route::get('/dashboard/expenses', GetExpensesController::class)->middleware([
+    'auth:sanctum'
+])->name('api.get-expenses');
